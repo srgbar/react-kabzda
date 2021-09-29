92192550c5c0fad7b64ac43739f10790774377fa
srgbar <srg.bar@yandex.ru>
2021-09-30 01:33:29 +0300

    add Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,44 +1,39 @@
 import React, {useState} from 'react';
-import {SelectHard, SelectPropsType} from "./Select";
+import {Select} from "./Select";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Select',
-    component: SelectHard,
+    component: Select,
 }
 
-export const SelectOff = () => {
-    return <SelectHard titleValue={"What are you studying now?"}
-                       onChange={() => {
-                       }}
-                       items={[]}
-    />
+export const WithValue = () => {
+    const [value, setValue] = useState("2")
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {title: "JS", value: "1"},
+                    {title: "React", value: "2"},
+                    {title: "HTML", value: "3"},
+                    {title: "CSS", value: "4"}
+                ]}/>
+    </>
 }
 
-export const SelectOn = () => {
-    return <SelectHard titleValue={"What are you studying now?"}
-                       onChange={() => {
-                       }}
-                       items={[
-                           {title: "JS", value: 1},
-                           {title: "React", value: 2},
-                           {title: "HTML", value: 3},
-                           {title: "CSS", value: 4}
-                       ]}
-    />
-}
-
-export const SelectMode = (props: SelectPropsType) => {
-
+export const WithoutValue = () => {
     const [value, setValue] = useState(undefined)
-
-    return <SelectHard titleValue={"What are you studying now?"}
-                       onChange={() => alert("Change skill")}
-                       items={[
-                           {title: "JS", value: 1},
-                           {title: "React", value: 2},
-                           {title: "HTML", value: 3},
-                           {title: "CSS", value: 4}
-                       ]}
-    />
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {title: "JS", value: "1"},
+                    {title: "React", value: "2"},
+                    {title: "HTML", value: "3"},
+                    {title: "CSS", value: "4"}
+                ]}/>
+    </>
 }
+
+
 
